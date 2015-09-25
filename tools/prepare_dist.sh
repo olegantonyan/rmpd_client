0e61fa9e7c713cd0ca0006e82ce90f509cf91055
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-09-25 05:36:57 +0200

    Bump to 0.0.4

--- tools/prepare_dist.sh
+++ tools/prepare_dist.sh
@@ -5,7 +5,7 @@ SOURCE=.
 
 rm -rf $DESTINATION
 
-rsync -av --exclude='.git' --exclude='rmpd_client' --exclude='doc/' --exclude='.settings/' --exclude='tmp/' $SOURCE $DESTINATION
+rsync -av --exclude='.git' --exclude='rmpd_client' --exclude='doc/' --exclude='.settings/' --exclude='tmp/' --exclude='.idea' $SOURCE $DESTINATION
 
 find $DESTINATION -name __pycache__ -type d -print0 | xargs -0 rm -r --
 find $DESTINATION -name '*.db3' -print0 | xargs -0 rm -r --
@@ -16,6 +16,7 @@ find $DESTINATION -name 'watchdogfile' -print0 | xargs -0 rm -r --
 find $DESTINATION -name '.project' -print0 | xargs -0 rm -r --
 find $DESTINATION -name '.pydevproject' -print0 | xargs -0 rm -r --
 find $DESTINATION -name '.gitignore' -print0 | xargs -0 rm -r --
+find $DESTINATION -name '*.pyc' -print0 | xargs -0 rm -r --
 
 VERSION=$(python3 -c 'import version; print(version.VERSION)')
 echo $VERSION
