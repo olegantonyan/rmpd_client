9018f1d11eeb495bac1f370c58e805334eb28b2c
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-12-03 06:50:56 +0200

    Stubs for new playback state machine

---
+++ mediaplayer/playback/states/base_state.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+class BaseState(object):
+    def __init__(self):
+        pass
---
+++ mediaplayer/playback/states/playing.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import mediaplayer.playback.states.base_state as base
+
+
+class Playing(base.BaseState):
+    pass
