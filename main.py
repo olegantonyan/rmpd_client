b9e9a1f53c99711bc1fcf8e246428cc30151727c
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-12-15 06:28:59 +0200

    Organizing imports in main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from time import sleep
-from sys import stdout, exit
-from logging import basicConfig, getLogger, StreamHandler, Formatter, info, DEBUG, INFO, warning, debug, critical
-from optparse import OptionParser
-from traceback import format_exc
-from signal import signal, SIGTERM
-from os import chdir, getcwd
+import time
+import sys
+import logging
+import optparse
+import traceback
+import signal
+import os
 
-import mediaplayer.playercontroller
-import remotecontrol.protocoldispatcher
+import mediaplayer.playercontroller as playerctrl
+import remotecontrol.protocoldispatcher as protoctrl
 import utils.config
 import utils.daemon
 import utils.threads
@@ -20,50 +20,50 @@ import system.watchdog
 
 
 def signal_handler(signum, frame):
-    info("caught signal {s}".format(s=signum))
-    mediaplayer.playercontroller.PlayerController().quit()
+    logging.info("caught signal {s}".format(s=signum))
+    playerctrl.PlayerController().quit()
     hardware.platfrom.set_all_leds_disabled()
-    warning("terminated")
-    exit(0)
+    logging.warning("terminated")
+    sys.exit(0)
 
 
 def setup_logger(console_app=False, verbose_log=False):
-    basicConfig(filename=utils.config.Config().logfile(),
-                format="[%(asctime)s] %(name)s |%(levelname)s| %(message)s",
-                datefmt="%Y-%m-%d %H:%M:%S",
-                level=(DEBUG if (verbose_log or utils.config.Config().verbose_logging()) else INFO))
+    logging.basicConfig(filename=utils.config.Config().logfile(),
+                        format="[%(asctime)s] %(name)s |%(levelname)s| %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S",
+                        level=(logging.DEBUG if (verbose_log or utils.config.Config().verbose_logging()) else logging.INFO))
 
     if console_app:
-        root_logger = getLogger()
-        child_logger = StreamHandler(stdout)
-        child_logger.setLevel(DEBUG)
-        formatter = Formatter("[%(asctime)s] %(name)s |%(levelname)s| %(message)s", "%Y-%m-%d %H:%M:%S")
+        root_logger = logging.getLogger()
+        child_logger = logging.StreamHandler(sys.stdout)
+        child_logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("[%(asctime)s] %(name)s |%(levelname)s| %(message)s", "%Y-%m-%d %H:%M:%S")
         child_logger.setFormatter(formatter)
         root_logger.addHandler(child_logger)
-        info("started as console application")
+        logging.info("started as console application")
     else:
-        info("started as daemon")
+        logging.info("started as daemon")
 
 
 def bootstrap(configfile, console_app=False, verbose_log=False):
     utils.config.Config().set_configfile(configfile)
     setup_logger(console_app, verbose_log)
-    info("using config file: '{c}'".format(c=configfile))
-    debug("working directory: '{w}'".format(w=getcwd()))
-    signal(SIGTERM, signal_handler)
+    logging.info("using config file: '{c}'".format(c=configfile))
+    logging.debug("working directory: '{w}'".format(w=os.getcwd()))
+    signal.signal(signal.SIGTERM, signal_handler)
 
 
 def app():
-    player = mediaplayer.playercontroller.PlayerController()
+    player = playerctrl.PlayerController()
     player.start_playlist()
-    proto = remotecontrol.protocoldispatcher.ProtocolDispatcher()
+    proto = protoctrl.ProtocolDispatcher()
     utils.threads.run_in_thread(webui.webui.start, ['0.0.0.0', 8080])
     while True:
         track = player.current_track_name()
         pos = player.current_track_posiotion()
         proto.send('now_playing', track=track, percent_position=pos)
         system.watchdog.Watchdog().feed()
-        sleep(20)
+        time.sleep(20)
 
 
 class DaemonApp(utils.daemon.Daemon):
@@ -73,15 +73,15 @@ class DaemonApp(utils.daemon.Daemon):
     def run(self):
         try:
             if self.cwd is not None:
-                chdir(self.cwd)
-            debug("daemon working directory: '{w}'".format(w=getcwd()))
+                os.chdir(self.cwd)
+            logging.debug("daemon working directory: '{w}'".format(w=os.getcwd()))
             app()
         except Exception as e:
-            critical("fatal: unhandled exception\n{e}\n{t}".format(e=str(e), t=format_exc()))
+            logging.critical("fatal: unhandled exception\n{e}\n{t}".format(e=str(e), t=traceback.format_exc()))
 
 
 def main():
-    parser = OptionParser()
+    parser = optparse.OptionParser()
     parser.add_option("-c", "--config-file", dest="configfile", help="path to configration file")
     parser.add_option("-w", "--working-dir", dest="workingdir", help="working directory in daemon mode")
     parser.add_option("-p", "--pid-file", dest="pidfile", help="path to pid file (only for daemon mode with -d option)")
@@ -95,21 +95,21 @@ def main():
         bootstrap(opts.configfile if opts.configfile else "rmpd.conf",
                   True,
                   opts.verbose)
-        exit(app())
+        sys.exit(app())
     else:
         if not opts.pidfile:
             parser.error("no pid file specified for daemon mode")
-            exit(2)
+            sys.exit(2)
 
         daemon = DaemonApp(opts.pidfile)
         if 'start' == opts.daemon_control:
             if not opts.configfile:
                 parser.error("no config file specified")
-                exit(2)
+                sys.exit(2)
             if not opts.workingdir:
                 parser.error("no working directory specified in daemon mode")
-                exit(2)
-            chdir(opts.workingdir)  # in main app context
+                sys.exit(2)
+            os.chdir(opts.workingdir)  # in main app context
             daemon.set_working_dir(opts.workingdir)
             bootstrap(opts.configfile, False, opts.verbose)
             daemon.start()
@@ -119,11 +119,11 @@ def main():
             daemon.status()
         elif not opts.daemon_control:
             parser.error("no daemon control options specified, try start or stop".format(opts.daemon_control))
-            exit(2)
+            sys.exit(2)
         else:
             parser.error("unknown daemon control command '{}'".format(opts.daemon_control))
-            exit(2)
-        exit(0)
+            sys.exit(2)
+        sys.exit(0)
 
 if __name__ == '__main__':
     main()
